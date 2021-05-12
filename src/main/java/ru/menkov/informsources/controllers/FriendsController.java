9ee145c9c7b7465b8bfc1c1ab159b11714203373
Ivan <menkov_99@mail.ru>
2021-05-13 02:37:24 +0300

    add Request body

--- src/main/java/ru/menkov/informsources/controllers/FriendsController.java
+++ src/main/java/ru/menkov/informsources/controllers/FriendsController.java
@@ -2,6 +2,7 @@ package ru.menkov.informsources.controllers;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.menkov.informsources.services.FriendsService;
@@ -17,13 +18,13 @@ public class FriendsController {
     }
 
     @RequestMapping("/friends/add")
-    public String addFriends(String inputJson){
+    public String addFriends(@RequestBody String inputJson){
         log.info("Request to add friends, body= {" + inputJson + "}");
         return friendsService.addFriends(inputJson);
     }
 
     @RequestMapping("/friends/delete")
-    public String deleteFriends(String inputJson){
+    public String deleteFriends(@RequestBody String inputJson){
         log.info("Request to delete friends, body= {" + inputJson + "}");
         return friendsService.deleteFriends(inputJson);
     }
