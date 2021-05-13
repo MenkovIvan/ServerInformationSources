aec94fde26e0fd678f3d6bd62fab7c08cf8a27b1
Ivan <menkov_99@mail.ru>
2021-05-13 03:26:45 +0300

    add @Param

--- src/main/java/ru/menkov/informsources/repositories/UserRepository.java
+++ src/main/java/ru/menkov/informsources/repositories/UserRepository.java
@@ -3,6 +3,7 @@ package ru.menkov.informsources.repositories;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.menkov.informsources.model.custom.User;
 
@@ -13,6 +14,6 @@ public interface UserRepository extends CrudRepository<User,Integer> {
     Boolean existsUserByEmailAndPassword(String email, String password);
     Boolean existsUserById(Integer id);
     @Modifying
-    @Query("update inf_sources.users u set u.online = ?1")
-    void setUserInfoById(Boolean online);
+    @Query("update inf_sources.users u set u.online = :online")
+    void setUserInfoById(@Param("online") Boolean online);
 }
