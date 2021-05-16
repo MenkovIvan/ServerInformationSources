f94300d6c4233cd20e6418adceb88c80eb903cf3
Ivan <menkov_99@mail.ru>
2021-05-16 13:58:32 +0300

    add Source.class

---
+++ src/main/java/ru/menkov/informsources/model/product/Source.java
@@ -0,0 +1,66 @@
+package ru.menkov.informsources.model.product;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "source", schema = "inf_sources")
+public class Source {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private Integer userId;
+    private Integer catalogId;
+
+    private String name;
+    private String description;
+    private String value;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Integer getCatalogId() {
+        return catalogId;
+    }
+
+    public void setCatalogId(Integer catalogId) {
+        this.catalogId = catalogId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
